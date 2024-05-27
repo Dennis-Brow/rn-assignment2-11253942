8a716d7b6a88812ff84ff05aca05cdfa502d7bab
Dennis-Brow <dsablordeppey@st.ug.edu.gh>
2024-05-27 05:39:36 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>My name is <Text style={styles.bold}>Dennis-Brown</Text></Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'grey', // Change the background color here
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24, // Increase the font size
+  },
+  bold: {
+    fontWeight: 'bold', // Make the name bold
+  },
+});
